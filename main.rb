11d5afaa13920e6aa4c1a2d14c9c77c980992271
RonnieGuo <guorongfly@gmail.com>
2023-10-22 21:28:25 -0600

    Modify main.rb to use the IOHandler class for I/O

--- main.rb
+++ main.rb
@@ -1,25 +1,27 @@
 require_relative 'player'
 require_relative 'game'
+require_relative 'io_handler'
+
 # Initialize the game and player
 player = Player.new
 game = Game.new(player)
 
 # Main game loop
 loop do
-  puts "Welcome to the Text Adventure Game!"
-  puts "Choose an option:"
-  puts "1. Start the game"
-  puts "2. Quit"
+  IOHandler.display("Welcome to the Text Adventure Game!")
+  IOHandler.display("Choose an option:")
+  IOHandler.display("1. Start the game")
+  IOHandler.display("2. Quit")
 
-  choice = gets.chomp
+  choice = IOHandler.get_input
 
   case choice
   when '1'
     game.start
   when '2'
-    puts "Thanks for playing!"
+    IOHandler.display("Thanks for playing!")
     break
   else
-    puts "Invalid choice. Please select a valid option."
+    IOHandler.display("Invalid choice. Please select a valid option.")
   end
 end
